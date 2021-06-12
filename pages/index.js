d94237c6652539235b3b22066134ab904eae088a
DavidV <d.veltzer@gmail.com>
2021-06-12 13:09:28 +0400

    added to intro

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Blog about being out there.</p>
+        <p>Blog about being out there. Btw, where exactely is 'there'?</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
